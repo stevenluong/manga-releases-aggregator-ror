133ba98bd1d342d5cf58395fe23b2b2079576976
stevenluong <ste.luong@gmail.com>
2013-08-20 03:04:26 +0200

    gitignore undo files / subscriptions impr

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,7 @@
 class Subscription < ActiveRecord::Base
   attr_accessible :manga_id, :user_id
+	belongs_to :user
+	belongs_to :manga
 	def self.get(user,manga)
 		Subscription.where(:manga_id=>manga.id,:user_id=>user.id).first
 	end
