71baa77d30a76dfcc9e9d070fb39c74b03a0ad8c
stevenluong <ste.luong@gmail.com>
2013-09-08 22:32:20 +0200

    devise custom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 	def out
     if !user_signed_in? || !current_user.is_admin
-      redirect_to root_path, notice: "well tried, but no"
+      redirect_to root_path, alert: "Well tried ! But No."
 		end
 	end	
 end
